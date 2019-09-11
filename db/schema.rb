022fb8650f1e813dc806bbe882f3b26b89f03202
vibraniumforge <gregm9999@gmail.com>
2019-09-11 12:00:30 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
-    t.integer "artist_id"
-    t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "artist_id"
+    t.integer "genre_id"
   end
 
 end
